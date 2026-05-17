e9428ee199eb57534519ea1ab95c3ab4255c3dbd
joelyakubu269 <joelyakubu269@gmail.com>
2026-05-17 23:30:28 +0100

    new files added

--- render.go
+++ render.go
@@ -1,18 +1,17 @@
 package main
 
-import "strings"
-
+import (
+	"strings"
+)
 func RenderLine(text string, banner map[rune][]string) []string {
-	result := make([]string, 8)
-	for i := 0; i < 8; i++ {
-		var rows strings.Builder // this is done to create a new empty builder for each row
-		// we dont want the rows overlapping
-		for _, c := range text {
-			if val, ok := banner[c]; ok {
+	result:= make([]string, 8)
+	for i:=0; i< 8; i++ {
+		var rows Strings.Builder
+		for _,c:= range text {
+			if val,ok := banner[c]; ok {
 				rows.WriteString(val[i])
 			}
 		}
-		result[i] = rows.String() // i is needed in order to access each of the character row by row
+		result[i]= rows.String
 	}
-	return result
 }
---
+++ split.go
@@ -0,0 +1,8 @@
+package main
+
+import(
+	"strings"
+)
+func split(text string) []string {
+	return strings.Split(text, "/n")
+}
