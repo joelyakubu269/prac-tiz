213fe99928100f8d7044dd1539861a39502c92d5
joelyakubu269 <joelyakubu269@gmail.com>
2026-05-18 00:37:02 +0100

    trying to generate ascii art

--- generate.go
+++ generate.go
@@ -1,6 +1,31 @@
 package main
 
-import(
+import (
 	"strings"
 )
-func 
+
+func GenerateArt(input string, banner map[rune][]string) string {
+	if input == "" {
+		return ""
+	}
+	err := validate(input)
+	if err != nil {
+		return err.Error() // bad practice tho just for the purpose of recoding
+	}
+	lines := split(input)
+	var results strings.Builder
+
+	for i, line := range lines {
+		if line == "" {
+			if i < len(lines)-1 {
+				results.WriteString("/n")
+			}
+			continue
+		}
+		rows := RenderLine(line, banner)
+		for _, row := range rows {
+			results.WriteString(row + "\n")
+		}
+	}
+	return results.String()
+}
