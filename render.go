368a22d98dcc5eceaaa1037ee79541ca337b20a5
joelyakubu269 <joelyakubu269@gmail.com>
2026-05-17 23:31:29 +0100

    new files added

--- render.go
+++ render.go
@@ -3,15 +3,16 @@ package main
 import (
 	"strings"
 )
+
 func RenderLine(text string, banner map[rune][]string) []string {
-	result:= make([]string, 8)
-	for i:=0; i< 8; i++ {
-		var rows Strings.Builder
-		for _,c:= range text {
-			if val,ok := banner[c]; ok {
+	result := make([]string, 8)
+	for i := 0; i < 8; i++ {
+		var rows strings.Builder
+		for _, c := range text {
+			if val, ok := banner[c]; ok {
 				rows.WriteString(val[i])
 			}
 		}
-		result[i]= rows.String
+		result[i] = rows.String
 	}
 }
