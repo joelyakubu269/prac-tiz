1416accf991d1e95049a2c4e8021f52df25c682a
joelyakubu269 <joelyakubu269@gmail.com>
2026-05-17 23:48:13 +0100

    new files added

--- render.go
+++ render.go
@@ -7,12 +7,13 @@ import (
 func RenderLine(text string, banner map[rune][]string) []string {
 	result := make([]string, 8)
 	for i := 0; i < 8; i++ {
-		var rows strings.Builder
+		var rows strings.Builder // creates a new empty builder every iteration, so that rows dont overlap
 		for _, c := range text {
 			if val, ok := banner[c]; ok {
 				rows.WriteString(val[i])
 			}
 		}
-		result[i] = rows.String
+		result[i] = rows.String() // collects the characters row by row
 	}
+	return result
 }
