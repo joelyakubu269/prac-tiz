ec6cafde8ffdfce3f15d9c53503b939530b246d6
joyakubu <joelyakubu269@gmail.com>
2026-05-21 03:38:20 +0100

    generate art using input string and banner

--- generate.go
+++ generate.go
@@ -6,11 +6,18 @@ func GenerateArt(input string, banner map[rune][]string) string {
 	if input == "" {
 		return ""
 	}
+	var result string
 	var art strings.Builder
 	lines := split(input)
 	for i, line := range lines {
-		if line == " " {
-
+		if line == " " { // this is for new line character as it was not considered in RenderLine function
+			art.WriteString("\n")
+			continue
 		}
+		rows := RenderLine(line, banner)
+		for _, row := range rows {
+			art.WriteString(row + "\n")
+		}
+		result[i]
 	}
 }
