d4d02ee9be720c3f82baae4d2b8b2c4fcb6fdcba
joyakubu <joelyakubu269@gmail.com>
2026-05-21 03:00:09 +0100

    recreating bannerfile

--- banner.go
+++ banner.go
@@ -10,15 +10,14 @@ func loadBanner(filename string) (map[rune][]string, error) {
 	m := make(map[rune][]string)
 	data, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error loading banner")
 	}
-	text := string(data)
-	charlines := strings.Split(text, "/n")
+	charlines := strings.Split(string(data), "\n")
 	for i := 32; i <= 126; i++ {
 		index := i - 32
 		start := index * 9
 		if start+8 > len(charlines) {
-			fmt.Errorf("invalid banner format")
+			return nil, fmt.Errorf("invalid banner format")
 		}
 		m[rune(i)] = charlines[start : start+8]
 	}
