a5aff40311703329146cb7ea16fe8987a9c6da0d
joyakubu <joelyakubu269@gmail.com>
2026-05-21 03:26:55 +0100

    generate art using input string and banner

--- generate.go
+++ generate.go
@@ -1,8 +1,16 @@
 package main
 
+import "strings"
+
 func GenerateArt(input string, banner map[rune][]string) string {
 	if input == "" {
 		return ""
 	}
+	var art strings.Builder
 	lines := split(input)
+	for i, line := range lines {
+		if line == " " {
+
+		}
+	}
 }
